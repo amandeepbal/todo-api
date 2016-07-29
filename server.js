bb8fd9ba1d284b678dc0872c0959a85fc85902a7
Amandeep Bal <amandeepbal@gmail.com>
2016-07-29 10:34:07 -0400

    Added Query Parameters

--- server.js
+++ server.js
@@ -13,15 +13,21 @@ app.get('/', function(req,res){
     res.send('Todo API Root');
 })
 
-// GET /todos?completed=true
+// GET /todos?completed=true&q=house
 app.get('/todos', function(req,res){
     var queryParams = req.query;
     var filteredTodos = todos;
-    
+// completed status
     if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true'){
         filteredTodos = _.where(filteredTodos,{completed: true})
     }else if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
         filteredTodos = _.where(filteredTodos,{completed: false})
+    }
+// query parameter
+    if(queryParams.hasOwnProperty('q') && queryParams.q.length > 0){
+    filteredTodos = _.filter(filteredTodos, function(todo){
+        return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+    })
     }
     
     res.json(filteredTodos);
