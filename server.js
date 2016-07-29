5321a4c0b92d4b4e622cde36e686eacfc4e6267d
Amandeep Bal <amandeepbal@gmail.com>
2016-07-28 21:53:27 -0400

    added POST for todos

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser')
+;
 var app = express ();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-    id: 1,
-    description:'Meet dad for lunch',
-    completed: false
-}, {
-    id: 2,
-    description:'Go to market',
-    completed: false
-}, {
-    id: 3,
-    description:'Feed the cat',
-    completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function(req,res){
     res.send('Todo API Root');
@@ -40,6 +33,19 @@ app.get('/todos/:id',function(req,res){
     }else{
         res.status(404).send();
     }
+})
+
+// POST /todo
+app.post('/todos',function(req,res){
+    var body = req.body;
+    
+// add incrementer and add field
+    body.id = todoNextId++;
+
+    // append to array
+    todos.push(body);    
+
+    res.json(body);
 })
 
 app.listen(PORT, function(){
