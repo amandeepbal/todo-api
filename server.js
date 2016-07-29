78d6f1adcd71a68472261eddfa49aa22fb8bfc58
Amandeep Bal <amandeepbal@gmail.com>
2016-07-29 09:40:18 -0400

    testing bracket

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 
-var app = express ();
+var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
 var todoNextId = 1;
