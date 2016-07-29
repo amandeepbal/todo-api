25809cdb3f9acb2d7e396a751d93bfd1df9afbca
Amandeep Bal <amandeepbal@gmail.com>
2016-07-28 22:40:28 -0400

    add DELETE to todo API

--- server.js
+++ server.js
@@ -11,16 +11,16 @@ app.use(bodyParser.json());
 
 app.get('/', function(req,res){
     res.send('Todo API Root');
-})
+});
 
 // GET /todos
 app.get('/todos', function(rer,res){
     res.json(todos);
-})
+});
 
 // GET /todos/:id
 app.get('/todos/:id',function(req,res){
-    var todoId = parseInt(req.params.id);
+    var todoId = parseInt(req.params.id, 10);
     var matchedTodo = _.findWhere(todos,{id:todoId});
 
     if(matchedTodo){
@@ -28,7 +28,7 @@ app.get('/todos/:id',function(req,res){
     }else{
         res.status(404).send();
     }
-})
+});
 
 // POST /todo
 app.post('/todos',function(req,res){
@@ -49,8 +49,22 @@ app.post('/todos',function(req,res){
     todos.push(body);    
 
     res.json(body);
-})
+});
+
+// DELETE
+app.delete('/todos/:id', function(req,res){
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos,{id:todoId});
+
+    if(matchedTodo){
+        todos = _.without(todos,matchedTodo);
+        res.json(matchedTodo);
+    }else{
+        res.status(404).json({"error":"No Todo found with id " + todoId});
+    }
+    
+});
 
 app.listen(PORT, function(){
     console.log('Express listneing on port ' + PORT +'!');
-})
+});
