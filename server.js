b3949e4d7c8b87a75ce493139536675446c69cbd
Amandeep Bal <amandeepbal@gmail.com>
2016-07-28 23:24:07 -0400

    add DELETE todo

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ app.use(bodyParser.json());
 
 app.get('/', function(req,res){
     res.send('Todo API Root');
-});
+})
 
 // GET /todos
 app.get('/todos', function(rer,res){
     res.json(todos);
-});
+})
 
 // GET /todos/:id
 app.get('/todos/:id',function(req,res){
@@ -28,7 +28,7 @@ app.get('/todos/:id',function(req,res){
     }else{
         res.status(404).send();
     }
-});
+})
 
 // POST /todo
 app.post('/todos',function(req,res){
@@ -49,9 +49,9 @@ app.post('/todos',function(req,res){
     todos.push(body);    
 
     res.json(body);
-});
+})
 
-// DELETE
+// DELETE /todos/:id
 app.delete('/todos/:id', function(req,res){
     var todoId = parseInt(req.params.id, 10);
     var matchedTodo = _.findWhere(todos,{id:todoId});
@@ -62,8 +62,36 @@ app.delete('/todos/:id', function(req,res){
     }else{
         res.status(404).json({"error":"No Todo found with id " + todoId});
     }
+})
+
+// PUT /todos/:id
+app.put('/todos/:id', function(req,res){
+    console.log('Express listneing on port ' + PORT +'!');
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos,{id:todoId});
+    var body = _.pick(req.body,'description','completed');
+    var validAttributes = {};
     
-});
+    if(!matchedTodo){
+        return res.status(404).send();        
+    }
+    
+    if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+            validAttributes.completed = body.completed;
+       }else if(body.hasOwnProperty('completed')){
+            res.status(404).send();
+       }
+    
+    if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+            validAttributes.description = body.description;
+       }else if(body.hasOwnProperty('description')){
+            res.status(404).send();
+       }
+    
+    _.extend(matchedTodo,validAttributes);
+     res.json(validAttributes);
+    
+})
 
 app.listen(PORT, function(){
     console.log('Express listneing on port ' + PORT +'!');
